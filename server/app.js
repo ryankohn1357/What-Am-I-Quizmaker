2a39b6d390c13ba51e6daf28474f4e5d9f4043b4
Ryan Kohn (RIT Student) <rtk1357@ad.rit.edu>
2019-11-01 12:27:51 -0400

    completed domo-maker c

--- server/app.js
+++ server/app.js
@@ -8,6 +8,9 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
+const redisStore = require('connect-redis')(session);
+const url = require('url');
+const csrf = require('csurf');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -20,26 +23,54 @@ mongoose.connect(dbURL, (err) => {
   }
 });
 
+let redisURL = {
+  hostname: 'redis-13011.c9.us-east-1-4.ec2.cloud.redislabs.com', // hostname from RedisLabs
+  port: '13011', // port number from RedisLabs
+};
+
+let redisPass = 'uI9DaHzjZUvyKSslqy1kOCcIFfzFf2Xy'; // password from RedisLabs
+
+if(process.env.REDISCLOUD_URL) {
+  redisURL = url.parse(process.env.REDISCLOUD_URL);
+  redisPass = redisURL.auth.split(':')[1];
+}
+
 // pull in our routes
 const router = require('./router.js');
 
 const app = express();
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.disable('x-powered-by');
 app.use(compression());
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
 app.use(session({
   key: 'sessionid',
+  store: new redisStore({
+    host: redisURL.hostname,
+    port: redisURL.port,
+    pass: redisPass,
+  }),
   secret: 'Domo Arigato',
   resave: true,
   saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+  },
 }));
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 app.use(cookieParser());
+app.use(csrf()); // must come after cookieparser and session and before router
+app.use((err, req, res, next) => {
+  if(err.code !== 'EBADCSRFTOKEN') return next(err);
+
+  console.log('Missing CSRF token');
+  return false;
+});
 
 router(app);
 
